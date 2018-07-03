049f5c48b1e9709c364f53f89a923d6b21e28cb6
Jon Exume <jonexume@gmail.com>
2018-07-03 23:55:13 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,13 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 cell = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
-row = ["   ", |, "   ", |, "   "]
+
 
 def display_board
-  row = " #{cell[0]} | #{cell[1]} | #{cell[2]} "
+  print = " #{cell[0]} | #{cell[1]} | #{cell[2]} "
   print = "-----------"
-  row = " #{cell[3]} | #{cell[4]} | #{cell[5]} "
+  print = " #{cell[3]} | #{cell[4]} | #{cell[5]} "
   print = "-----------"
-  row = " #{cell[6]} | #{cell[7]} | #{cell[8]} "
+  print = " #{cell[6]} | #{cell[7]} | #{cell[8]} "
 end
 
 display_board(cell)
