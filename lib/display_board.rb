7e48e8cab6c6a04a5851c0831e620a2827b40858
Jon Exume <jonexume@gmail.com>
2018-07-03 23:45:06 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,11 +2,11 @@
 cell = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
 
 def display_board
-  puts = " #{cell[0]} | #{cell[1]} | #{cell[2]} "
-  puts = "-----------"
-  puts = " #{cell[3]} | #{cell[4]} | #{@cell[5]} "
-  puts = "-----------"
-  puts = " #{cell[6]} | #{cell[7]} | #{cell[8]} "
+  print = " #{cell[0]} | #{cell[1]} | #{cell[2]} "
+  print = "-----------"
+  print = " #{cell[3]} | #{cell[4]} | #{@cell[5]} "
+  print = "-----------"
+  print = " #{cell[6]} | #{cell[7]} | #{cell[8]} "
 end
 
 display_board(cell)
