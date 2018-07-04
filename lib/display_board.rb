dfec24fac60794e8358303b91cab466cfcb00202
Jon Exume <jonexume@gmail.com>
2018-07-04 00:02:25 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 cell = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
 
 
-def display_board(board)
+def display_board
   print  " #{cell[0]} | #{cell[1]} | #{cell[2]} "
   print  "-----------"
   print  " #{cell[3]} | #{cell[4]} | #{cell[5]} "
